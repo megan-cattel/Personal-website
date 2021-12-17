00e7aa5b7feb984aa7988d09968b4967f4b8ae07
haoshuai999 <626593718@qq.com>
2021-12-17 12:27:14 -0500

    add new tab fact-checking

---
+++ src/components/FactCheck.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function FactCheck() {
+  return (
+    <div>
+        <div className="row justify-content-center p-3">
+            <div className="col col-12 col-md-8 article">
+                <div className="article-title">
+                    <p><b><i>Fact-Checking</i></b></p>
+                </div>
+                <ul>
+                    <li>
+                        <a href="https://www.npr.org/2021/10/08/1044267583/first-female-principal-chief-of-the-cherokee-nation-will-be-featured-on-u-s-coin">First female principal chief of the Cherokee Nation will be featured on US coin</a> (StoryCorps, NPR Morning Edition)
+                    </li>
+                    <li>
+                        <a href="https://storycorps.org/stories/i-dont-break-how-two-people-found-strength-in-one-another-after-police-violence/">“I don't break”: How Two People Found Strength in One Another After Police Violence</a> (StoryCorps, NPR Morning Edition)
+                    </li>
+                    <li>
+                        <a href="https://www.kansas.com/news/local/article255975537.html#storylink=cpy">Damon Young and Martin Garcia have a conversation on One Small Step in Wichita</a> (StoryCorps, The Wichita Eagle)
+                    </li>
+                    <li>
+                        <a href="https://www.npr.org/2021/12/10/1062598719/santas-macys-aids-awareness-protest">The day Santas stormed Macy's to protest for AIDS awareness</a> (StoryCorps, NPR Morning Edition)
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default FactCheck;
