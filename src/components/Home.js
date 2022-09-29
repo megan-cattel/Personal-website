f321be727d74bd58a4ffa69a1464a390535f90c6
haoshuai999 <626593718@qq.com>
2022-09-28 21:44:23 -0400

    change one word in the sentence

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ function Home() {
                     <p className="p-2"><b>→ Here are links to my published <a href={process.env.PUBLIC_URL + "/#/writing"}>writing</a>, <a href={process.env.PUBLIC_URL + "/#/audio"}>audio</a> and <a href={process.env.PUBLIC_URL + "/#/fact-checking"}>fact-checking</a> work.</b></p>
                   </div> 
                     <p className="p-2">↳ My reporting can be found at <a href="https://www.wbur.org/inside/staff/megan-cattel">WBUR</a>, <a href="https://www.teenvogue.com/contributor/megan-cattel">Teen Vogue</a>, <a href="https://www.goldthread2.com/author/megan-cattel">Goldthread</a> (a South China Morning Post imprint), <a href="https://news.stlpublicradio.org/people/megan-cattel">St. Louis Public Radio</a>, <a href="https://supchina.com/author/megancattel/">SupChina</a>, and <a href="https://megancattel.contently.com/">elsewhere</a>. I’ve also taken research assignments for iHeartMedia’s podcast division.</p>
-                    <p className="p-2">↳ I serve as board member and head podcast editor at <a href="https://nuvoices.com/">NüVoices</a>, a collective which supports marginalized genders publishing work about China, Taiwan, and Hong Kong.</p>
+                    <p className="p-2">↳ I serve as board member and head podcast editor at <a href="https://nuvoices.com/">NüVoices</a>, a collective which supports marginalized voices publishing work about China, Taiwan, and Hong Kong.</p>
                     <p className="p-2">Thank you for visiting my website!</p>
                     <br />
                     <br />
