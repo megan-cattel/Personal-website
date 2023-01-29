a8c1086c26746b2df76fd0a8c3ffa3fdab0a4b0a
haoshuai999 <626593718@qq.com>
2023-01-28 22:36:16 -0500

    change the order of episodes and font size

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ function About() {
                   <li>Since 2021, I’ve served as podcast editor at NüVoices, a collective which supports underrepresented voices publishing work about China, Taiwan, and Hong Kong. To date, I’ve <a href="https://nuvoices.com/category/podcast/">edited and produced over 20 episodes for this indie podcast!</a></li>
                   <li>I am a <a href="https://pulitzercenter.org/people/megan-cattel">2022 Pulitzer Center Environmental Science Reporting Fellow</a>.</li>
                   <li>I'm half-Chinese and wrote an essay about being biracial in both China and America <a href="https://nuvoices.com/2019/10/29/navigating-my-biracial-identity-as-a-chinese-american-living-in-china/">here</a>.</li>
-                  <li>Before journalism, I was a teacher, au pair, UN intern, and competitive figure skater.  (<a href="https://www.youtube.com/watch?v=ME4ICyXSoFs">Here are some underrated figure skating performances</a> I think you should check out!) </li>
+                  <li>Before journalism, I was a teacher, au pair, UN intern, and competitive figure skater.  (<a href="https://www.youtube.com/watch?v=xsGyPUXHrNA&t=275s">Here</a> <a href="https://www.youtube.com/watch?v=oMWGkZsncec">are some</a> <a href="https://www.youtube.com/watch?v=eENh7cMpktk&t=42s">underrated</a> <a href="https://www.youtube.com/watch?v=T4SDsRTJ33s">figure skating</a> <a href="https://www.youtube.com/watch?v=ME4ICyXSoFs">performances</a> <a href="https://www.youtube.com/watch?v=662TbCAqLRw">I think</a> you should check out!) </li>
                 </ul>
                 <br />
                 <br />
--- src/components/Audio.js
+++ src/components/Audio.js
@@ -8,11 +8,8 @@ function Audio() {
                     <div className="article-title">
                         <p><b><i>Longform Audio Features</i></b></p>
                     </div>
+                    <p className="small">Below are longform audio stories I pitched, reported, produced, and scripted for WBUR, Boston’s NPR station. As web producer, I also transcribed and designed the layout of these episodes.</p>
                     <ul>
-                        <li>
-                            <a href="https://open.spotify.com/episode/0ITmcCeSr1Pav3eRXelYmr?si=2Lw5GFDvRcG8BhjRw-5o8A">Goosebumps: The little (not so scary) musical that’s trying to get onto Broadway</a> (WBUR)
-                        </li>
-                        <iframe src="https://open.spotify.com/embed/episode/0ITmcCeSr1Pav3eRXelYmr?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         <li>
                             <a href="https://www.wbur.org/endlessthread/2022/11/18/life-death-ai">Life, death, and AI: Can AI help us better understand grief?</a> (WBUR)
                         </li>
@@ -25,34 +22,6 @@ function Audio() {
                             <a href="https://www.wbur.org/endlessthread/2022/07/29/alyssa">Sex work, ‘normal work’, and men: a profile</a> (WBUR)
                         </li>
                         <iframe src="https://open.spotify.com/embed/episode/4I79l7RgTXfjIw9rVJmWSJ?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                        <li>
-                            <a href="https://www.wbur.org/endlessthread/2022/07/01/aunties-network">‘Hope resides in connection’: Abortion access advocates on navigating a post-Roe world</a> (WBUR)
-                        </li>
-                        <iframe src="https://open.spotify.com/embed/episode/2NuiMMZ2p9r5GkUZWQaJoG?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                    </ul>
-                </div>
-            </div>
-            <div className="row justify-content-center p-3">
-                <div className="col col-12 col-md-8 article">
-                    <div className="article-title">
-                        <p><b><i>On the Mic</i></b></p>
-                    </div>
-                    <ul>
-                        <li>
-                            <a href="https://www.wbur.org/considerthis/2022/10/28/norwood-skate-america-competition">Diversity and mental health in figure skating with Gracie Gold</a> (Consider This, WBUR)
-                        </li>
-                        <li>
-                            <a href="https://nuvoices.com/2022/11/16/nuvoices-podcast-85-becoming-a-stand-up-comedian-with-he-huang/">A Conversation with He Huang: How a Chinese Comedian went viral (and riled up nationalists) around the world</a> (NüVoices)
-                        </li>
-                        <iframe src="https://open.spotify.com/embed/episode/2gBkdKB1690ptERhF9LRl4?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                        <li>
-                            <a href="https://nuvoices.com/2022/06/02/nuvoices-podcast-76-a-conversation-with-elaine-hsieh-chou-on-her-debut-novel-disorientation/">Elaine Hsieh Chou on her debut novel Disorientation: They hate us but pretend to be us</a> (NüVoices)
-                        </li>
-                        <iframe src="https://open.spotify.com/embed/episode/2c7p1u1KCeqeRcZZbSMz6a?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                        <li>
-                            <a href="https://nuvoices.com/2022/03/17/nuvoices-podcast-72-messy-roots-a-conversation-with-laura-gao/">A Conversation with Laura Gao: A Wuhanese-American on Covid-19, coming out as queer, and breaking down internalized racism in her new memoir Messy Roots</a> (NüVoices) 
-                        </li>
-                        <iframe src="https://open.spotify.com/embed/episode/6IKjJ1dELg5Xsa0YuyXZHk?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                     </ul>
                 </div>
             </div>
@@ -61,7 +30,16 @@ function Audio() {
                     <div className="article-title">
                         <p><b><i>Podcast Production Work</i></b></p>
                     </div>
+                    <p className="small">Below are audio stories I helped produce for WBUR and Foreign Policy. I cut tape, booked guests, contacted fixers, found archival sound, and went out into the field to record interviews.</p>
                     <ul>
+                        <li>
+                            <a href="https://open.spotify.com/episode/0ITmcCeSr1Pav3eRXelYmr?si=2Lw5GFDvRcG8BhjRw-5o8A">Goosebumps: The little (not so scary) musical that’s trying to get onto Broadway</a> (WBUR)
+                        </li>
+                        <iframe src="https://open.spotify.com/embed/episode/0ITmcCeSr1Pav3eRXelYmr?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        <li>
+                            <a href="https://www.wbur.org/endlessthread/2022/07/01/aunties-network">‘Hope resides in connection’: Abortion access advocates on navigating a post-Roe world</a> (WBUR)
+                        </li>
+                        <iframe src="https://open.spotify.com/embed/episode/2NuiMMZ2p9r5GkUZWQaJoG?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         <li>
                             <a href="https://foreignpolicy.com/podcasts/negotiators/?episode=how-a-motley-group-of-negotiators-freed-the-chibok-schoolgirls">Bring Back Our Girls: How Negotiators Freed the Chibok Schoolgirls</a> (The Negotiators, Foreign Policy)
                         </li>
@@ -83,8 +61,43 @@ function Audio() {
             <div className="row justify-content-center p-3">
                 <div className="col col-12 col-md-8 article">
                     <div className="article-title">
+                        <p><b><i>Editing</i></b></p>
+                    </div>
+                    <p className="small">Since August 2021, I’ve been head editor of the <a href="https://nuvoices.com/">NüVoices</a> podcast, a show which elevates marginalized genders who publish work about China. We regularly platform journalists, diplomats, academics, writers, and artists from around the world to diversify English-language points of view on China, Taiwan, and Hong Kong. To date, I’ve produced and edited over 20 episodes for this biweekly podcast series. I also book guests, prep our team of hosts, and offer tech support over Riverside FM whenever needed. Mostly, I edit down the podcast and cut tape in Adobe Audition. Occasionally, I also host episodes! (Scroll down to learn more.) </p>
+                    <iframe id="player_iframe" src="https://www.buzzsprout.com/1858406?client_source=large_player&iframe=true&referrer=https%3A%2F%2Fwww.buzzsprout.com%2F1858406%2Fpodcast%2Fembed" loading="lazy" width="100%" height="375" frameborder="0" scrolling="no" title="NüVoices"></iframe>
+                </div>
+            </div>
+            <div className="row justify-content-center p-3">
+                <div className="col col-12 col-md-8 article">
+                    <div className="article-title">
+                        <p><b><i>On the Mic</i></b></p>
+                    </div>
+                    <p className="small">One of the reasons why I fell in love with audio: I listened to Terry Gross on <i>Fresh Air</i> religiously as a kid! While I mostly do podcast production work, I also go behind the mic and host episodes too. Below are some of my favorites.</p>
+                    <ul>
+                        <li>
+                            <a href="https://nuvoices.com/2022/11/16/nuvoices-podcast-85-becoming-a-stand-up-comedian-with-he-huang/">A Conversation with He Huang: How a Chinese Comedian went viral (and riled up nationalists) around the world</a> (NüVoices)
+                        </li>
+                        <iframe src="https://open.spotify.com/embed/episode/2gBkdKB1690ptERhF9LRl4?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        <li>
+                            <a href="https://nuvoices.com/2022/06/02/nuvoices-podcast-76-a-conversation-with-elaine-hsieh-chou-on-her-debut-novel-disorientation/">Elaine Hsieh Chou on her debut novel Disorientation: They hate us but pretend to be us</a> (NüVoices)
+                        </li>
+                        <iframe src="https://open.spotify.com/embed/episode/2c7p1u1KCeqeRcZZbSMz6a?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        <li>
+                            <a href="https://nuvoices.com/2022/03/17/nuvoices-podcast-72-messy-roots-a-conversation-with-laura-gao/">A Conversation with Laura Gao: A Wuhanese-American on Covid-19, coming out as queer, and breaking down internalized racism in her new memoir Messy Roots</a> (NüVoices) 
+                        </li>
+                        <iframe src="https://open.spotify.com/embed/episode/6IKjJ1dELg5Xsa0YuyXZHk?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        <li>
+                            <a href="https://www.wbur.org/considerthis/2022/10/28/norwood-skate-america-competition">Diversity and mental health in figure skating with Gracie Gold</a> (Consider This, WBUR)
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row justify-content-center p-3">
+                <div className="col col-12 col-md-8 article">
+                    <div className="article-title">
                         <p><b><i>Reported Enterprise Stories</i></b></p>
                     </div>
+                    <p className="small">I pitched, reported, and produced the following stories for Uptown Radio, Columbia Journalism School’s weekly newsmagazine show.</p>
                     <ul>
                         <li>
                             <a href="https://uptownradio.wixsite.com/uptownradio/post/volunteers-step-up-to-distribute-food-for-nyc-s-seniors">Volunteers Step Up to Distribute Food for NYC’s Elderly Chinese Community</a> (Uptown Radio)
